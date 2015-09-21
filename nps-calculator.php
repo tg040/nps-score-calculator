336589c392c7ac1c265dae0133d2dabbd67099c7
tg040 <tghilardi@hotmail.com>
2015-09-21 10:24:16 -0300

    first commit

---
+++ nps-calculator.php
@@ -0,0 +1,31 @@
+/**
+ * @param array
+ * @return float
+ */
+function output_real_nps($nps_array)
+{
+    $detractors=0;
+    $promoters=0;
+
+    $arr_length=count($nps_array);
+
+    for ($i=0;$i<$arr_length;$i++)
+    {
+        if (is_numeric($nps_array[$i]))
+        {
+            if ($nps_array[$i] <= 6 && $nps_array[$i] >= 0)
+                $detractors++;
+            if ($nps_array[$i] >= 9 && $nps_array[$i] <= 10)
+                $promoters++;
+        }
+    }
+
+    // div by 0 not allowed
+    if ($arr_length>0)
+      $nps_score=(($promoters-$detractors)/$arr_length)*100;
+    else
+        $nps_score='0';
+
+    return round($nps_score,1);
+
+}
