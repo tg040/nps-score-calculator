baa36e270afa1a7785e03f9052aac01304f61744
Thiago Ghilardi <tghilardi@hotmail.com>
2016-04-07 19:15:49 -0300

    Create nps-calculator-oop.php

---
+++ nps-calculator-oop.php
@@ -0,0 +1,39 @@
+<?
+
+/**
+ * Class NpsCalculator
+ * OOP way
+ */
+class NpsCalculator {
+
+    public $detractors;
+    public $promoters;
+    public $npsScore;
+
+    public function returnNpsScore($npsArray) {
+        $arrLength=count($npsArray);
+        for ($i=0; $i<=$arrLength; $i++) {
+            // let`s make sure it is numeric
+            if (is_numeric($npsArray[$i])) {
+                if ($npsArray[$i]<=6 && $npsArray[$i]>=0)
+                    $this->detractors++;
+                if ($npsArray[$i]>= 9 && $npsArray[$i]<=10)
+                    $this->promoters++;
+            }
+        }
+        // if array is empty, then return 0 since div by 0 is not allowed
+        if ($arrLength>0)
+            $this->npsScore=(($this->promoters - $this->detractors)/$arrLength)*100;
+        else
+            $this->npsScore='0';
+
+        return round($this->npsScore,1);
+    }
+}
+
+$npsScore = new NpsCalculator;
+$npsArray=array(0,3,4,10,10,4,10,10,10,10);
+
+echo 'NPS score: '.$npsScore->returnNpsScore($npsArray).'<br />';
+
+?>
